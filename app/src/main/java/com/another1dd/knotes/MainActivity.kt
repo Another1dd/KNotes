27db780697d4feb3e46645d46dd53eeb3e5caf5f
Timophei <tima@testking.ru>
2017-05-25 10:56:35 +0300

    Initial commit

---
+++ app/src/main/java/com/another1dd/knotes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.another1dd.knotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
